45de9411c870e724410f20b36c1bd6ceb0542fa3
Denis Lazarev <mahrizh@gmail.com>
2026-02-13 21:31:16 +0200

    TypeDropdownAttribute docs

--- Runtime/TypeDropdownAttribute.cs
+++ Runtime/TypeDropdownAttribute.cs
@@ -5,27 +5,36 @@ using UnityEngine;
 
 namespace TypeDropdown
 {
+	/// <summary>
+	/// Attribute to show a dropdown of types in the inspector.
+	/// Can be applied to <c>[SerializeReference]</c> object or <c>[SerializeField]</c> string.
+	/// </summary>
 	[AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
 	public class TypeDropdownAttribute : PropertyAttribute
 	{
 		public readonly string? NamePattern;
 		public readonly Type[]? BaseTypes;
 
+		/// <inheritdoc cref="TypeDropdownAttribute(string, Type[])"/>
 		public TypeDropdownAttribute()
 			: this((string?)null, null)
 		{
 		}
 
+		/// <inheritdoc cref="TypeDropdownAttribute(string, Type[])"/>
 		public TypeDropdownAttribute(string namePattern)
 			: this(namePattern, null)
 		{
 		}
 
+		/// <inheritdoc cref="TypeDropdownAttribute(string, Type[])"/>
 		public TypeDropdownAttribute(params Type[] baseTypes)
 			: this(null, baseTypes)
 		{
 		}
 
+		/// <param name="namePattern"> Regular expression pattern to filter type names </param>
+		/// <param name="baseTypes"> Base classes or interfaces to filter types. </param>
 		public TypeDropdownAttribute(string? namePattern, params Type[]? baseTypes)
 		{
 			NamePattern = namePattern;
